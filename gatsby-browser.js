f8fa3a665e9694816a63ac5b676c94b7197e9d68
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-09-23 14:58:42 +0300

    Add global styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,20 @@
-require("prismjs/themes/prism.css")
+import "prismjs/themes/prism.css"
+
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    #gatsby-noscript {
+        display: none;
+    }
+    body {
+        background: #fafafa;
+    }
+`;
+
+export const wrapRootElement = ({ element }) => (
+  <>
+    <GlobalStyle />
+    {element}
+  </>
+);
