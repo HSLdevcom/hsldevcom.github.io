0ff09b76eff18a3f07f33d850bc6d7956101ce3a
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-09-23 14:50:19 +0300

    Create container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,20 @@
 import React from "react"
 import styled from "styled-components"
 
+import Header from "./Header"
+
 const Container = styled.main`
-    margin: 4rem auto;
-    max-width: 1200px;
+  margin: 2rem auto;
+  max-width: 1200px;
+  padding: 0.5rem;
+  background: white;
+  border: 1px solid gray;
+  border-radius: 4px;
 `
 
 export default ({ children }) => (
-    <Container>{ children }</Container>
+  <>
+    <Header />
+    <Container>{children}</Container>
+  </>
 )
