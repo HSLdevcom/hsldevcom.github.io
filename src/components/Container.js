e0bedc5bb9cb6c6d7acbcf815a25decdccc674d0
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-09-26 12:50:53 +0300

    Create separate component for content to add margin and fix content overflowing

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,18 +3,27 @@ import styled from "styled-components"
 
 import Header from "./Header"
 
-const Container = styled.main`
-  margin: 2rem auto;
+const Container = styled.div`
+  margin: 2rem 2rem;
+`
+
+const Content = styled.main`
+  margin: 0 auto;
   max-width: 1200px;
   padding: 1.75rem;
   background: white;
   border: 1px solid lightgray;
   border-radius: 4px;
+  overflow: hidden;
 `
 
 export default ({ children }) => (
   <>
     <Header />
-    <Container>{children}</Container>
+    <Container>
+      <Content>
+        {children}
+      </Content>
+    </Container>
   </>
 )
