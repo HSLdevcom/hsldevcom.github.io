1917b874ca4e6804b91aecc78e122fa506afa730
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-09-26 12:37:05 +0300

    Increase padding

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,7 @@ import Header from "./Header"
 const Container = styled.main`
   margin: 2rem auto;
   max-width: 1200px;
-  padding: 0.5rem;
+  padding: 1.75rem;
   background: white;
   border: 1px solid lightgray;
   border-radius: 4px;
