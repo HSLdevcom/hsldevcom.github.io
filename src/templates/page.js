61f5ebbbfd1d62aa899a4e1325c9e5aa44ed2b5c
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-12-05 15:16:55 +0200

    Make tables in Markdown scrollable

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,16 +1,24 @@
 import React from "react"
+import styled from "styled-components"
 import { graphql } from "gatsby"
 
 import Container from "../components/Container"
 import SEO from "../components/SEO";
 import Breadcrumbs from "../components/Breadcrumbs";
 
+const Markdown = styled.div`
+  & table {
+    display: block;
+    overflow-x: auto;
+  }
+`
+
 export default ({ data, pageContext }) => (
   <Container>
       <Breadcrumbs breadcrumbs={[ ...pageContext.breadcrumbs, { path: pageContext.slug, title: data.markdownRemark.frontmatter.title }]} />
       <SEO pageTitle={data.markdownRemark.frontmatter.title} pageDescription={data.markdownRemark.excerpt} pagePath={pageContext.slug} />
       <h1>{ data.markdownRemark.frontmatter.title }</h1>
-      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+      <Markdown dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
   </Container>
 )
 
